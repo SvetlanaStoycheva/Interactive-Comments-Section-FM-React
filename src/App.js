bbbbdea1b46eb1b5d8b1dd04b1d147d06297242b
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-12-23 15:48:26 +0200

    data is visualized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,106 @@
 import React from 'react';
-import data from './data.json';
+import { data } from './data';
+import { BsPlus, BsArrow90DegLeft } from 'react-icons/bs';
+import { FiMinus } from 'react-icons/fi';
+import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 
 function App() {
-  console.log(data.comments);
+  // console.log(data.comments);
 
   return (
-    <main>
-      <section className='comments-container'></section>
+    <main className='main'>
+      <section className='comments-container'>
+        {data.comments.map((item) => {
+          const { content, createdAt, id, replies, score, user } = item;
+          const {
+            image: { png: userImage },
+            username,
+          } = user;
+          console.log(replies);
+          // Comments
+          return (
+            <>
+              <article className='single-comment' key={id}>
+                <div className='votes-btn'>
+                  <button>
+                    <span>
+                      <BsPlus />
+                    </span>
+                    <p>{score}</p>
+                    <span>
+                      <FiMinus />
+                    </span>
+                  </button>
+                </div>
+                <div className='info-container'>
+                  <div className='info-header'>
+                    <div className='img-name-container'>
+                      <img src={userImage} alt='user' />
+                      <h3>{username}</h3>
+                      <h4>{createdAt}</h4>
+                    </div>
+                    <div className='replay-btn'>
+                      <button>
+                        <span>
+                          <BsArrow90DegLeft />
+                        </span>
+                        Replay
+                      </button>
+                    </div>
+                  </div>
+                  <p>{content}</p>
+                </div>
+              </article>
+              {/* Replaies */}
+              {replies.length > 0 && (
+                <div className='replies-container'>
+                  {replies.map((item, index) => {
+                    const { content, createdAt, id, score, user } = item;
+                    const {
+                      image: { png: userImage },
+                      username,
+                    } = user;
+                    //  single replay container
+                    return (
+                      <article className='single-comment' key={`${id}${index}`}>
+                        <div className='votes-btn'>
+                          <button>
+                            <span>
+                              <BsPlus />
+                            </span>
+                            <p>{score}</p>
+                            <span>
+                              <FiMinus />
+                            </span>
+                          </button>
+                        </div>
+                        <div className='info-container'>
+                          <div className='info-header'>
+                            <div className='img-name-container'>
+                              <img src={userImage} alt='user' />
+                              <h3>{username}</h3>
+                              <h4>{createdAt}</h4>
+                            </div>
+                            <div className='replay-btn'>
+                              <button>
+                                <span>
+                                  <BsArrow90DegLeft />
+                                </span>
+                                Replay
+                              </button>
+                            </div>
+                          </div>
+                          <p>{content}</p>
+                        </div>
+                      </article>
+                    );
+                  })}
+                </div>
+              )}
+            </>
+          );
+        })}
+      </section>
     </main>
   );
 }
