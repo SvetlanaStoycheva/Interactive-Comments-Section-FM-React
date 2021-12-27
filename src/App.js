c13c9fb4edad256ff7284ed7ce5fff020d1137a4
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-12-27 19:21:40 +0200

    html and css completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { data } from './data';
 import { BsPlus, BsArrow90DegLeft } from 'react-icons/bs';
 import { FiMinus } from 'react-icons/fi';
 import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
+import img from './images-avatars/image-juliusomo.png';
 
 function App() {
   // console.log(data.comments);
@@ -14,7 +15,7 @@ function App() {
           const { content, createdAt, id, replies, score, user } = item;
           const {
             image: { png: userImage },
-            username,
+            username: usernameMainComment,
           } = user;
           console.log(replies);
           // Comments
@@ -22,25 +23,31 @@ function App() {
             <>
               <article className='single-comment' key={id}>
                 <div className='votes-btn'>
-                  <button>
-                    <span>
+                  <button className='score-btn'>
+                    <span className='score-btn-icons score-btn-icons-plus '>
                       <BsPlus />
                     </span>
                     <p>{score}</p>
-                    <span>
+                    <span className='score-btn-icons'>
                       <FiMinus />
                     </span>
+                  </button>
+                  <button className='replay-btn-small-window'>
+                    <span className='replay-btn-icon'>
+                      <BsArrow90DegLeft />
+                    </span>
+                    Replay
                   </button>
                 </div>
                 <div className='info-container'>
                   <div className='info-header'>
                     <div className='img-name-container'>
                       <img src={userImage} alt='user' />
-                      <h3>{username}</h3>
+                      <h3>{usernameMainComment}</h3>
                       <h4>{createdAt}</h4>
                     </div>
-                    <div className='replay-btn'>
-                      <button>
+                    <div>
+                      <button className='replay-btn-big-window'>
                         <span>
                           <BsArrow90DegLeft />
                         </span>
@@ -62,16 +69,25 @@ function App() {
                     } = user;
                     //  single replay container
                     return (
-                      <article className='single-comment' key={`${id}${index}`}>
+                      <article
+                        className='single-comment single-replay'
+                        key={`${id}${index}`}
+                      >
                         <div className='votes-btn'>
-                          <button>
-                            <span>
+                          <button className='score-btn'>
+                            <span className='score-btn-icons score-btn-icons-plus '>
                               <BsPlus />
                             </span>
                             <p>{score}</p>
-                            <span>
+                            <span className='score-btn-icons'>
                               <FiMinus />
                             </span>
+                          </button>
+                          <button className='replay-btn-small-window'>
+                            <span className='replay-btn-icon'>
+                              <BsArrow90DegLeft />
+                            </span>
+                            Replay
                           </button>
                         </div>
                         <div className='info-container'>
@@ -81,8 +97,8 @@ function App() {
                               <h3>{username}</h3>
                               <h4>{createdAt}</h4>
                             </div>
-                            <div className='replay-btn'>
-                              <button>
+                            <div>
+                              <button className='replay-btn-big-window'>
                                 <span>
                                   <BsArrow90DegLeft />
                                 </span>
@@ -90,7 +106,12 @@ function App() {
                               </button>
                             </div>
                           </div>
-                          <p>{content}</p>
+                          <p>
+                            <span className='main-comment-name'>
+                              @{usernameMainComment}{' '}
+                            </span>
+                            {content}
+                          </p>
                         </div>
                       </article>
                     );
@@ -100,6 +121,27 @@ function App() {
             </>
           );
         })}
+        {/* Create comment container */}
+        <article className='form-container'>
+          <img src={img} alt='author' className='send-form-img-big-screen' />
+          <form className='form'>
+            <textarea
+              type='text'
+              className='form-input'
+              placeholder='Add a comment...'
+            />
+            <div className='send-btn-container'>
+              <img
+                src={img}
+                alt='author'
+                className='send-form-img-small-screen'
+              />
+              <button type='submit' className='submit-btn'>
+                SEND
+              </button>
+            </div>
+          </form>
+        </article>
       </section>
     </main>
   );
